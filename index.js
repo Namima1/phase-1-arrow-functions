996b6921c07233ab436683e8b202fab3bd8aca05
Namima Islam <namimaislam308@gmail.com>
2024-06-11 16:07:05 -0400

    complete lab

---
+++ index.js
@@ -0,0 +1,19 @@
+//has a function expression called divide
+//divide divides 2000 by 100
+
+//has an arrow function called square
+//square arrow function takes one parameter and multiplies it times itself
+
+//has an arrow function called add
+//add arrow function takes two parameters and adds them together
+
+const divide = () => 2000 / 100;
+divide();
+
+
+const square = (num) => num * num;
+square(100); 
+
+
+const add = (num1, num2) => num1 + num2;
+add(100, 200);
